871f6f53a4820f48e15860786d0303cc1fe01154
Sewdohe <sewdohe@protonmail.com>
2024-07-22 22:12:54 -0400

    make generated images public b/c discord requires image urls, not raw data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const puppet = require('./puppet')
 const axios = require('axios')
+const path = require('path')
 
 const corsOptions = {
   origin: 'http://localhost:8000',
@@ -10,7 +11,7 @@ const corsOptions = {
 
 const app = express();
 app.use(cors(corsOptions));
-app.use(express.static('public'))
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 const port = 3876;
 app.listen(port, () => {
