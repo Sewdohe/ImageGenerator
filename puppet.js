460ab67ae004296b84cdad565b8ab191f8569c16
Sewdohe <sewdohe@protonmail.com>
2024-07-23 16:54:28 -0400

    hopefully fix the 'navigating frame disconnected error`

--- puppet.js
+++ puppet.js
@@ -11,7 +11,7 @@ exports.generateHelloImage = async function (req, res) {
 
   const browser = await puppeteer.launch({
     executablePath: '/usr/bin/google-chrome',
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-features=site-per-process']
   });
 
   const page = await browser.newPage();
